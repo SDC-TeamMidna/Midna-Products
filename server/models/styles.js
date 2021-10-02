b9606559b245eb792403f5693e90156ddb9453f8
Andy Fang <afang1094@gmail.com>
2021-10-01 18:28:14 -0700

    deleted styles object

--- server/models/styles.js
+++ server/models/styles.js
@@ -2,10 +2,6 @@ const db = require('../db');
 
 module.exports = {
   get: (params, callback) => {
-    const styles = {
-      product_id: params,
-      result: [],
-    };
     const text = `SELECT product_id, JSON_AGG(JSON_BUILD_OBJECT(
       'style_id', styles.id, 'name', name, 'sale_price', sale_price,
       'original_price', original_price, 'default?', styles.default, 'photos',
@@ -23,7 +19,6 @@ module.exports = {
         WHERE styles.product_id = $1
         GROUP BY product_id;`;
 
-    // const photo = 'SELECT * FROM photos WHERE product_id = $1';
     db.query(text, params)
       .then((data) => callback(null, data.rows))
       .catch((err) => callback(err, null));
