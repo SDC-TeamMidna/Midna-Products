bb7153fe2a176b7baf34f06f7a64d3ee845b2d9c
Andy Fang <afang1094@gmail.com>
2021-10-01 18:24:24 -0700

    implemented get method for styles

--- server/models/products.js
+++ server/models/products.js
@@ -30,22 +30,3 @@ module.exports = {
       .catch((err) => callback(err, null));
   },
 };
-
-/*
-SELECT
-reviews.id as review_id,
-rating,
-summary,
-recommend,
-body,
-to_char(date at time zone 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS"Z"') as date,
-reviewer_name,
-helpfulness,
-JSON_AGG(json_build_object('id', reviews_photos.id, 'url', reviews_photos.url))
-FROM public.reviews
-LEFT JOIN public.reviews_photos
-ON reviews.id = reviews_photos.review_id
-WHERE product_id=2
-GROUP BY reviews.id
-ORDER BY date DESC LIMIT 50
-*/
--- server/models/styles.js
+++ server/models/styles.js
@@ -6,15 +6,26 @@ module.exports = {
       product_id: params,
       result: [],
     };
-    const style = 'SELECT * FROM styles WHERE product_id = $1;';
+    const text = `SELECT product_id, JSON_AGG(JSON_BUILD_OBJECT(
+      'style_id', styles.id, 'name', name, 'sale_price', sale_price,
+      'original_price', original_price, 'default?', styles.default, 'photos',
+      (SELECT JSON_AGG(JSON_BUILD_OBJECT(
+        'thumbnail_url', thumbnail_url, 'url', url
+        ))
+        FROM photos WHERE photos.style_id = styles.id), 'skus',
+      (SELECT JSON_OBJECT_AGG(id,
+        JSON_BUILD_OBJECT('quantity', quantity, 'size', size))
+        AS skus
+        FROM skus
+        WHERE skus.style_id = styles.id
+        GROUP by style_id)))
+        AS results FROM styles
+        WHERE styles.product_id = $1
+        GROUP BY product_id;`;
+
     // const photo = 'SELECT * FROM photos WHERE product_id = $1';
-    db.query(style, params)
-      .then((data) => {
-        const { id } = data.rows[0];
-        db.query('SELECT * FROM photos WHERE style_id = $1;', [id])
-          .then((photos) => {
-            console.log(photos.rows);
-          });
-      });
+    db.query(text, params)
+      .then((data) => callback(null, data.rows))
+      .catch((err) => callback(err, null));
   },
 };
